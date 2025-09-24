884260173bab3785f6be7dca5736072891952d51
EatFan <eatfan0921@163.com>
2025-09-24 17:43:05 +0800

    feat: Tab栏切换页面功能

--- lib/pages/container_page.dart
+++ lib/pages/container_page.dart
@@ -10,22 +10,23 @@ class ContainerPage extends StatefulWidget {
 
 class _ContainerPageState extends State<ContainerPage> {
 
+  // Tab页面索引
   int _currentTabIndex = 0;
 
   // 主要页面列表
-  List pageList = [
-    Container(color: Colors.blue, child: Text("首页")),
-    Container(color: Colors.red, child: Text("商城")),
-    Container(color: Colors.green, child: Text("消息")),
-    Container(color: Colors.yellow, child: Text("我")),
+  final List<Widget> _pageList = [
+    Container(color: Colors.blue, child: Center(child: Text("首页"))),
+    Container(color: Colors.red, child: Center(child: Text("商城"))),
+    Container(color: Colors.green, child: Center(child: Text("消息"))),
+    Container(color: Colors.yellow, child: Center(child: Text("我"))),
   ];
 
+  // 选择tab，将当前索引设置选择的tab物品的索引
   void _selectedTabIndex(int index){
     setState(() {
       _currentTabIndex = index;
     });
-
-    print("currentTabIndex:" + _currentTabIndex.toString() + " index: " + index.toString());
+    print("currentTabIndex:$_currentTabIndex index: $index");
   }
 
   @override
@@ -33,17 +34,16 @@ class _ContainerPageState extends State<ContainerPage> {
     return Scaffold(
       body: Stack(
         children: [
-
           // 主内容区域
-          IndexedStack(
-            children: [
-              Container(
-                color: Colors.red,
-              )
-            ],
+          // 因为底部Tab栏使用Position部件，这里主内容区域使用Positioned.fill占满其余空间
+          Positioned.fill(
+            child: IndexedStack(
+              index: _currentTabIndex,
+              children: _pageList,
+            ),
           ),
-
           // 底部Tab栏
+          // 分装好的，内部最外层为Position部分
           BottomTabBar(
             onTabSelected: (index) => _selectedTabIndex(index),
           )
