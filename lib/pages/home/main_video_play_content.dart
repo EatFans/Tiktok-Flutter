e6d8d56105d0318cb91e26077e5242c84961092a
EatFan <eatfan0921@163.com>
2025-09-25 12:51:28 +0800

    feat: 视频自适应横屏竖屏

---
+++ lib/pages/home/main_video_play_content.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// 视频播放主要主体部分
+class MainVideoPlayContent extends StatefulWidget {
+  const MainVideoPlayContent({super.key});
+
+  @override
+  State<MainVideoPlayContent> createState() => _MainVideoPlayContentState();
+}
+
+class _MainVideoPlayContentState extends State<MainVideoPlayContent> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
--- lib/widgets/video_player_widget.dart
+++ lib/widgets/video_player_widget.dart
@@ -23,7 +23,7 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   @override
   void initState() {
     super.initState();
-    _videoPlayerController = VideoPlayerController.asset('assets/video/test1.mp4')
+    _videoPlayerController = VideoPlayerController.asset('assets/video/test2.mp4')
       ..initialize().then((_) {
         _playVideo();
       });
@@ -34,13 +34,18 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
     _videoPlayerController.setVolume(1.0);
     _videoPlayerController.play();
     _videoPlayerController.setLooping(true);
+    print("播放视频");
   }
 
   // 暂停视频
   void pauseVideo(){
-    setState(() {
+    if (_videoPlayerController.value.isPlaying){
       _videoPlayerController.pause();
-    });
+      print("暂停视频");
+    } else {
+      _videoPlayerController.play();
+      print("继续播放");
+    }
   }
 
   // 处置状态
@@ -55,12 +60,39 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
     if (!_videoPlayerController.value.isInitialized) {
       return Center(child: CircularProgressIndicator());
     }
-    return FittedBox(
-      fit: BoxFit.cover,
-      child: SizedBox(
-        width: _videoPlayerController.value.size.width,
-        height: _videoPlayerController.value.size.height,
-        child: VideoPlayer(_videoPlayerController),
+
+    final videoSize = _videoPlayerController.value.size; // 视频原始尺寸
+    final screenSize = MediaQuery.of(context).size;
+
+    final videoAspectRatio = videoSize.width / videoSize.height;
+
+    return GestureDetector(
+      onTap: pauseVideo,
+      child: Center(
+        child: videoAspectRatio >= 1
+        // 横屏视频
+          ? Transform.translate(
+              offset: const Offset(0, -20),
+              child: Container(
+                width: screenSize.width,
+                height: (screenSize.width / videoAspectRatio) * 0.8,
+                color: Colors.black,
+                child: VideoPlayer(_videoPlayerController),
+              ),
+          )
+        // 竖屏视频
+            : Container(
+          width: screenSize.width,
+          height: screenSize.height,
+          child: FittedBox(
+            fit: BoxFit.cover,
+            child: SizedBox(
+              width: videoSize.width,
+              height: videoSize.height,
+              child: VideoPlayer(_videoPlayerController),
+            ),
+          ),
+        ),
       ),
     );
   }
