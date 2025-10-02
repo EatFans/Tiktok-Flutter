6fe6e1d776c6bee84e172294993dd66db33754ec
EatFan <eatfan0921@163.com>
2025-10-02 14:34:04 +0800

    fix: 修改成无状态widget

--- lib/widgets/home/video_info_action.dart
+++ lib/widgets/home/video_info_action.dart
@@ -2,14 +2,13 @@
 
 import 'package:flutter/material.dart';
 
-class VideoInfoAction extends StatefulWidget {
-  const VideoInfoAction({super.key});
+class VideoInfoAction extends StatelessWidget {
 
-  @override
-  State<VideoInfoAction> createState() => _VideoInfoActionState();
-}
 
-class _VideoInfoActionState extends State<VideoInfoAction> {
+  const VideoInfoAction({
+    super.key
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,134 +20,62 @@ class _VideoInfoActionState extends State<VideoInfoAction> {
             SizedBox(
               width: 52,
               height: 52,
-              child: ClipRRect (
+              child: ClipRRect(
                 borderRadius: BorderRadius.circular(50),
                 child: Image.network(
                   "https://picx.zhimg.com/50/v2-6afa72220d29f045c15217aa6b275808_720w.jpg",
-                  fit: BoxFit.cover, // 图片填充整个容器
+                  fit: BoxFit.cover,
                 ),
               ),
             ),
-
             const SizedBox(height: 24),
-
             // 点赞
             GestureDetector(
-              onTap: (){
-                // 点赞逻辑
-              },
+              onTap: () {},
               child: Column(
-                children: [
-                  Container(
-                    width: 40,
-                    height: 38,
-                    child: const Icon(
-                      Icons.favorite,
-                      color: Colors.white,
-                      size: 30
-                    ),
-                  ),
-                  const Text(
-                    "1324",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold
-                    )
-                  ),
+                children: const [
+                  Icon(Icons.favorite, color: Colors.white, size: 30),
+                  SizedBox(height: 2),
+                  Text("1324", style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold)),
                 ],
               ),
             ),
-
             const SizedBox(height: 24),
-
             // 评论
             GestureDetector(
-              onTap: () {
-                // 评论逻辑
-              },
+              onTap: () {},
               child: Column(
-                children: [
-                  Container(
-                    width: 40,
-                    height: 38,
-                    child: const Icon(
-                      Icons.comment,
-                      color: Colors.white,
-                      size: 28,
-                    ),
-                  ),
-                  const Text(
-                    "322",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
+                children: const [
+                  Icon(Icons.comment, color: Colors.white, size: 28),
+                  SizedBox(height: 2),
+                  Text("322", style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold)),
                 ],
               ),
             ),
-
-            const SizedBox(height: 24,),
-
+            const SizedBox(height: 24),
             // 收藏
             GestureDetector(
-              onTap: () {
-
-              },
+              onTap: () {},
               child: Column(
-                children: [
-                  Container(
-                    width: 40,
-                    height: 38,
-                    child: const Icon(
-                      Icons.star,
-                      color: Colors.white,
-                      size: 30
-                    ),
-                  ),
-                  const Text(
-                    "220",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold
-                    )
-                  )
+                children: const [
+                  Icon(Icons.star, color: Colors.white, size: 30),
+                  SizedBox(height: 2),
+                  Text("220", style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold)),
                 ],
               ),
             ),
-
             const SizedBox(height: 24),
-
             // 分享
             GestureDetector(
-              onTap: (){
-
-              },
+              onTap: () {},
               child: Column(
-                children: [
-                  Container(
-                    width: 40,
-                    height: 38,
-                    child: const Icon(
-                      Icons.share,
-                      color: Colors.white,
-                      size: 28
-                    )
-                  ),
-                  const Text(
-                    "61",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold
-                    ),
-                  )
+                children: const [
+                  Icon(Icons.share, color: Colors.white, size: 28),
+                  SizedBox(height: 2),
+                  Text("61", style: TextStyle(color: Colors.white, fontSize: 12, fontWeight: FontWeight.bold)),
                 ],
               ),
-            )
+            ),
           ],
         ),
       ),
